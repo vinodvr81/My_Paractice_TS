ca62bda360eb7e8ab0b4391a8eed24b3b00c0473
vinodvr81 <vinod.vukkalam@gmail.com>
2020-10-26 18:26:52 +0530

    added menu files

--- course-starting-project/src/app/app.module.ts
+++ course-starting-project/src/app/app.module.ts
@@ -12,7 +12,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
-imports: [MatListModule,];
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+imports: [MatListModule, MatGridListModule,
+  MatCardModule,
+  MatButtonModule,];
 
 @NgModule({
   declarations: [
